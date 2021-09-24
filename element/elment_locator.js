1cb8ddcb6b17fbff17056509d86ffe5d958e3db9
Leonard Steinhoff <90336556+MicroNet-Foundation@users.noreply.github.com>
2021-09-24 22:08:50 +0200

    Using HTML Elements with "$"

---
+++ element/elment_locator.js
@@ -0,0 +1,24 @@
+
+function $(selector) {
+    const self = {
+        element: document.querySelector(selector),
+        html: () => self.element,
+        on: (event, callback) => {
+            document.addEventListener(event, callback);
+        },
+
+        hide: () => {
+            self.element.style.display = 'none';
+        },
+
+        attr: (name, value) => {
+            if (value == null) {
+                self.element.getAttribute(name);
+            } else {
+                self.element.setAttribute(name, value);
+            }
+        }
+    }
+
+    return self;
+}
---
+++ test.js
@@ -0,0 +1,1 @@
+$('Hi').attr('class', 'texts');
